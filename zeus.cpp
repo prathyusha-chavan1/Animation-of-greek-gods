d99a3115eca5ac9c2782b072703c7a4dafc429a6
PranavSrivatsa <pranav.srivatsa27@gmail.com>
2019-03-29 11:14:08 +0530

    zues body

---
+++ zeus.cpp
@@ -0,0 +1,208 @@
+#include<glut.h>
+#include<stdio.h>
+#include<math.h>
+
+float scale = 0.5;
+
+void myinit() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, 600, 0, 600);
+	glMatrixMode(GL_MODELVIEW);
+	glScalef(.5, .5, .5);
+	glTranslatef(500, 500, 0);
+}
+
+void display() {
+	float x1 = 150, y1 = 190, x2, y2;
+	float angle, radius = 10;
+	glClearColor(0, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	
+
+
+	//thunderbolt
+	glColor3f(1, 1, 0);
+	glBegin(GL_POLYGON);
+		glVertex2f(100 , 300);
+		glVertex2f(120 , 200);
+		glVertex2f(135 , 220);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+		glVertex2f(170, 100);
+		glVertex2f(150, 250);
+		glVertex2f(130, 230);
+	glEnd();
+
+	//hand
+	glColor3f(0.9, 0.8, 0.4);
+	glBegin(GL_POLYGON);
+	for (angle = 1.0f; angle < 361.0f; angle += 0.2)
+	{
+		x2 = x1 + sin(angle)*radius;
+		y2 = y1 + cos(angle)*radius;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+
+	glBegin(GL_POLYGON);
+		glVertex2f(150, 195);
+		glVertex2f(200, 240);
+		glVertex2f(210, 230);
+		glVertex2f(160, 190);
+	glEnd();
+
+	//right robe flap
+	glColor3f(0.8, 0.8, 0.8);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(387, 237);
+	glVertex2f(370, 90);
+	glColor3f(0.5, 0.5, 0.5);
+	glVertex2f(385, 92);
+	glColor3f(0.3, 0.3, 0.3);
+	glVertex2f(420, 95);
+	glEnd();
+
+	//robe lower
+	glColor3f(0.85, 0.85, 0.85);
+	glBegin(GL_POLYGON);
+		glVertex2f(210, 130);
+		glVertex2f(240, 190);
+		glVertex2f(245, 185);
+		glVertex2f(215, 125);
+	glEnd();
+	
+	glColor3f(0.8, 0.8, 0.8);
+	glBegin(GL_TRIANGLE_FAN);
+		glVertex2f(245, 185);
+		glVertex2f(207, 110);
+		glColor3f(0.5, 0.5, 0.5);
+		glVertex2f(242, 90);
+		glColor3f(0.2, 0.2, 0.2);
+		glVertex2f(243, 120);
+	glEnd();
+	
+	glColor3f(0.9, 0.9, 0.9);
+	glBegin(GL_POLYGON);
+	glVertex2f(245, 185);
+	glVertex2f(237, 70);
+	glVertex2f(265, 65);
+	glVertex2f(310, 75);
+	glVertex2f(365, 65);
+	glVertex2f(390, 70);
+	glVertex2f(382, 185);
+	glEnd();
+
+	//shading
+	glColor3f(0.7, 0.7, 0.7);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(245, 185);
+	glVertex2f(237, 70);
+	glColor3f(0.8, 0.8, 0.8);
+	glVertex2f(265, 65);
+	glColor3f(0.9, 0.9, 0.9);
+	glVertex2f(278, 100);
+
+	glVertex2f(300, 200);
+	glEnd();
+
+	//waist left
+	glColor3f(0.9, 0.8, 0.4);
+	glBegin(GL_POLYGON);
+	glVertex2f(239, 220);
+	glVertex2f(240, 190);
+	glVertex2f(245, 185);
+	glVertex2f(260, 184);
+	glVertex2f(245, 240);
+	glEnd();
+
+	//left crease
+	glColor3f(0.6, 0.6, 0.6);
+	glBegin(GL_POLYGON);
+	glVertex2f(300, 165);
+	glVertex2f(265, 65);
+	glVertex2f(295, 70);
+	glEnd();
+
+	//right crease
+	glColor3f(0.6, 0.6, 0.6);
+	glBegin(GL_POLYGON);
+	glVertex2f(320, 155);
+	glVertex2f(325, 70);
+	glVertex2f(355, 65);
+	glEnd();
+
+	//robe upper
+	glColor3f(0.8, 0.8, 0.8);
+	glBegin(GL_POLYGON);
+	glVertex2f(245, 238);
+	glVertex2f(260, 184);
+	glVertex2f(265, 170);
+	glVertex2f(300, 145);
+	glVertex2f(330, 135);
+	glVertex2f(385, 185);
+	glVertex2f(355, 240);
+	glEnd();
+
+	glBegin(GL_TRIANGLE_FAN);	
+	glColor3f(0.4, 0.4, 0.4);	
+	glVertex2f(388, 75);
+	glVertex2f(393, 77);	
+	glColor3f(0.6, 0.6, 0.6);
+	glVertex2f(398, 80);	
+	glColor3f(0.8, 0.8, 0.8);
+	glVertex2f(385, 185);
+	glVertex2f(330, 135);
+	glEnd();
+
+	//waist right
+	glColor3f(0.9, 0.8, 0.4);
+	glBegin(GL_POLYGON);
+	glVertex2f(355, 240);
+	glVertex2f(385, 185);
+	glVertex2f(390, 240);
+	//glVertex2f(390, 77);
+	glEnd();
+
+	//mid left crease
+	glColor3f(0.6, 0.6, 0.6);
+	glBegin(GL_POLYGON);
+	glVertex2f(280, 225);
+	glVertex2f(288, 210);
+	glVertex2f(320, 165);
+	glVertex2f(360, 145);
+	glVertex2f(330, 180);
+	glEnd();
+
+	//mid right crease
+	glColor3f(0.6, 0.6, 0.6);
+	glBegin(GL_POLYGON);
+	glVertex2f(310, 225);
+	glVertex2f(330, 200);
+	glVertex2f(360, 170);
+	glVertex2f(348, 200);
+	glEnd();
+
+	glFlush();
+}
+
+void reshape(int w, int h) {
+	if(w>=h)
+		glViewport(0, 0, (GLsizei)h, (GLsizei)h);
+	else
+		glViewport(0, 0, (GLsizei)w, (GLsizei)w);	
+}
+
+void main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitWindowPosition(100, 50);
+	glutInitWindowSize(1000, 600);
+	glutCreateWindow("Zues");
+	myinit();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+}
