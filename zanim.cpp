4888cff4ebd20e1b2053d380c646bff4f139dd76
PranavSrivatsa <pranav.srivatsa27@gmail.com>
2019-05-03 14:16:46 +0530

    zpage

--- zanim.cpp
+++ zanim.cpp
@@ -12,10 +12,9 @@ float bottomx12 = 760, bottomx22 = 710, bottomy12 = 582, bottomy22 = 572;
 bool flag = true, flag2 = true, flag3 = true, f1 = false, f2 = false;
 float ypos = 10, c5 = 0.1,C4 = 0.1, hy = 0, hx1 = 0;
 GLfloat alpha = 1;
-void drawCloud(int, int, int, int, int, int, int, double);
 float scale = 0.5, width, height;
 const float DEG2RAD = 3.14159 / 180;
-int ctr = 1;
+int ctr = 1,pages=1;
 
 void reset_ln() {
 	switchs = 1;
@@ -60,11 +59,6 @@ void drawCircleFilled(double centerX, double centerY, double radiusX, double rad
 void drawmouth()
 {
 	glColor3f(0, 0, 0);
-	/*glBegin(GL_LINES);
-	glVertex2i(305, 295);
-	glVertex2i(332, 295);
-	glEnd();
-	glFlush();*/
 	float x2, y2;
 	glBegin(GL_LINE_STRIP);
 	for (float angle = 250.0; angle < 291.0; angle += 0.2)
@@ -75,6 +69,7 @@ void drawmouth()
 	}
 	glEnd();
 }
+
 void drawface()
 {
 	glColor3f(0.96, 0.80, 0.69);
@@ -85,23 +80,6 @@ void drawface()
 	glVertex2i(415, 330);
 	glEnd();
 	glFlush();
-}
-
-
-void drawCloud(int xx, int yy, int p, int q, int r, int u, int v, double a)
-{
-	int x = xx, y = yy;
-
-	glColor3f(a, a, a);
-	drawCircleFilled(x, y, p, q);
-	glColor3f(a, a, a);
-	drawCircleFilled(x + u, y + v, r, r);
-	glColor3f(a, a, a);
-	drawCircleFilled(x + u, y - v, r, r);
-	glColor3f(a, a, a);
-	drawCircleFilled(x - u, y - v, r, r);
-	glColor3f(a, a, a);
-	drawCircleFilled(x - u, y + v, r, r);
 }
 
 void drawcloud() {
@@ -215,37 +193,61 @@ void drawlightning() {
 		//glLoadIdentity();
 	}
 }
+void resetpage2()
+{
+	c5 =0,ypos = 10, C4 = 0.1, hy = 0, hx1 = 0, f2 = f1 = false;;
+	glutPostRedisplay();
+}
 void update(int)
 {
 
 	glutPostRedisplay();
-	glutTimerFunc(1000 / 60, update, 0);
-
-	if (ypos < 600)
-		ypos += 10;
-	if (hy < 45)
-		hy += 1;
-	if (hx1 < 20)
-		hx1 += 1;
-	if (C4 < 0.80) {
-		C4 += 0.0035;
-		c5 += 0.0025;
+	glutTimerFunc(1000/60, update, 0);
+	if (pages == 2) {
+		if (ypos < 600)
+			ypos += 10;
+		if (hy < 45)
+			hy += 1;
+		if (hx1 < 20)
+			hx1 += 1;
+		if (C4 < 0.80) {
+			C4 += 0.0035;
+			c5 += 0.0025;
+		}
+		if (C4 >= 0.80)
+			f1 = true;
 	}
-	if (C4 >= 0.80)
-		f1 = true;
-
-
-
 }
+
+void drawbox1()
+{
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_POLYGON);
+	glVertex2f(1800, 50);
+	glVertex2f(1870, 50);
+	glVertex2f(1870, 80);
+	glVertex2f(1800, 80);
+	glEnd();
+	glFlush();
+}
+
 void myMouseFunc(int button, int state, int x, int y)
 {
 	cout << x << endl << y;
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && (x >= 580 && x <= 850) && (y >= 480 && y <= 810)) {
-		cout << x << endl << y;
-		f2 = true;
-		glutTimerFunc(0, update, 0);
-	}
 
+		f2 = true;
+		pages = 2;
+		glutTimerFunc(0, update, 0);
+		
+	}
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && (x >= 1439 && x <= 1495) && (y >= 798 && y <= 822)) {
+		pages = 1;
+		resetpage2();
+
+		
+	}
 }
 
 void drawclouds(int x3, int y3, float s1, float s2)
@@ -256,9 +258,6 @@ void drawclouds(int x3, int y3, float s1, float s2)
 
 	glTranslated(-250, 0, 0);
 	glScalef(s1, s2, 0);
-
-
-
 
 	glBegin(GL_POLYGON);
 
@@ -287,6 +286,25 @@ void drawclouds(int x3, int y3, float s1, float s2)
 	glLoadIdentity();
 	glFlush();
 }
+
+void drawfaceh()
+{
+
+	float hx = 25, x1 = hx + 150, y1 = 190, x2, y2;
+	float angle, radius = 10;
+
+	glColor3f(0.96, 0.80, 0.69);
+	glBegin(GL_POLYGON);
+
+	for (angle = 1.0; angle < 360; angle += 0.2)
+	{
+		x2 = 320 + sin(angle * DEG2RAD) * 95;
+		y2 = 330 + cos(angle * DEG2RAD) * 120;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+}
+
 
 void drawzeus() {
 	glTranslated(615, 0, 0);
@@ -560,6 +578,338 @@ void drawzeus() {
 	glLoadIdentity();
 	glFlush();
 }
+
+void drawhades()
+{
+	glTranslated(215, 0, 0);
+	float hx = 25, x1 = hx + 150, y1 = 190, x2, y2;
+	float angle, radius = 10;
+
+	glColor3f(0.65, 0.65, 0.65);
+	glBegin(GL_POLYGON);
+	glVertex2d(170 + 17, 70);
+	glVertex2d(180 + 17, 70);
+	glVertex2d(130 + 17, 310);
+	glVertex2d(120 + 17, 310);
+	glEnd();
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_POLYGON);
+	glVertex2d(130 + 17, 310);
+	glVertex2d(120 + 17, 310);
+	glVertex2d(123 + 17, 295);
+	glVertex2d(133 + 17, 295);
+	glEnd();
+
+	glColor3f(0.3, 0.3, 0.3);
+	glBegin(GL_POLYGON);
+	glVertex2d(120 + 17, 305);
+	glVertex2d(95 + 17, 305);
+	glVertex2d(95 + 17, 310);
+	glVertex2d(120 + 17, 310);
+	glEnd();
+
+
+	glBegin(GL_POLYGON);
+	glVertex2d(130 + 17, 305);
+	glVertex2d(152 + 17, 305);
+	glVertex2d(152 + 17, 310);
+	glVertex2d(130 + 17, 310);
+	glEnd();
+
+
+
+	glBegin(GL_POLYGON);
+	glVertex2d(95 + 17, 305);
+	glVertex2d(100 + 17, 305);
+	glVertex2d(95 + 17, 327);
+	glVertex2d(90 + 17, 327);
+	glEnd();
+
+
+
+	glBegin(GL_POLYGON);
+	glVertex2d(155 + 17, 305);
+	glVertex2d(150 + 17, 305);
+	glVertex2d(145 + 17, 330);
+	glVertex2d(150 + 17, 330);
+	glEnd();
+
+
+	glBegin(GL_TRIANGLES);
+	glVertex2d(141 + 17, 330);
+	glVertex2d(145 + 17, 345);
+	glVertex2d(155 + 17, 330);
+	glEnd();
+
+
+	glBegin(GL_TRIANGLES);
+	glVertex2d(87 + 17, 326);
+	glVertex2d(90 + 17, 342);
+	glVertex2d(100 + 17, 326);
+	glEnd();
+
+
+
+
+
+
+
+	//left hand
+	glColor3f(0.96, 0.80, 0.69);
+	glBegin(GL_POLYGON);
+	for (angle = 1.0f; angle < 361.0f; angle += 0.2)
+	{
+		x2 = (x1)+sin(angle) * radius;
+		y2 = (y1)+cos(angle) * radius;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex2f(150 + hx, 195);
+	glVertex2f(210 + hx, 250);
+	glVertex2f(220 + hx, 240);
+	glVertex2f(160 + hx, 190);
+	glEnd();
+
+	//right robe flap
+	glColor3f(0.2, 0, 0);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(387, 237);
+	glVertex2f(370, 90);
+	glColor3f(0.6, 0, 0);
+	glVertex2f(385, 92);
+	glColor3f(1, 0, 0);
+	glVertex2f(420, 95);
+	glEnd();
+
+	//right hand
+	glColor3f(0.96, 0.80, 0.69);
+	glBegin(GL_POLYGON);
+	glVertex2f(395, 240);
+	glVertex2f(405, 240);
+	glVertex2f(417, 160);
+	glVertex2f(405, 160);
+	glVertex2f(405, 200);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	for (angle = 1.0f; angle < 361.0f; angle += 0.2)
+	{
+		x2 = 405 + sin(angle) * 12;
+		y2 = 160 + cos(angle) * 12;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+	//robe lower
+	glColor3f(0.9, 0, 0);
+	glBegin(GL_POLYGON);
+	glVertex2f(210, 130);
+	glVertex2f(240, 190);
+	glVertex2f(245, 185);
+	glVertex2f(215, 125);
+	glEnd();
+
+	glColor3f(0.7, 0, 0);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(245, 185);
+	glVertex2f(207, 110);
+	glColor3f(0.6, 0, 0);
+	glVertex2f(242, 90);
+	glColor3f(0.8, 0, 0);
+	glVertex2f(243, 120);
+	glEnd();
+
+	glColor3f(0.2, 0.2, 0.2);
+	glBegin(GL_POLYGON);
+	glVertex2f(245, 185);
+	glVertex2f(237, 70);
+	glVertex2f(265, 65);
+	glVertex2f(310, 75);
+	glVertex2f(365, 65);
+	glVertex2f(390, 70);
+	glVertex2f(382, 185);
+	glEnd();
+
+	//shading
+	glColor3f(0.2, 0.2, 0.2);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(245, 185);
+	glVertex2f(237, 70);
+	glColor3f(0.3, 0.3, 0.3);
+	glVertex2f(265, 65);
+	glColor3f(0.4, 0.4, 0.4);
+	glVertex2f(278, 100);
+
+	glVertex2f(300, 200);
+	glEnd();
+
+	//waist left
+	glColor3f(0.96, 0.80, 0.69);
+	glBegin(GL_POLYGON);
+	glVertex2f(239, 220);
+	glVertex2f(240, 190);
+	glVertex2f(245, 185);
+	glVertex2f(260, 184);
+	glVertex2f(245, 240);
+	glEnd();
+
+	//left crease
+	glColor3f(0, 0, 0);
+	glBegin(GL_POLYGON);
+	glVertex2f(300, 165);
+	glVertex2f(265, 65);
+	glVertex2f(295, 70);
+	glEnd();
+
+	//right crease
+	glColor3f(0, 0, 0);
+	glBegin(GL_POLYGON);
+	glVertex2f(320, 155);
+	glVertex2f(325, 70);
+	glVertex2f(355, 65);
+	glEnd();
+
+	//robe upper
+	glColor3f(0.3, 0.3, 0.3);
+	glBegin(GL_POLYGON);
+	glVertex2f(245, 238);
+	glVertex2f(260, 184);
+	glVertex2f(265, 170);
+	glVertex2f(300, 145);
+	glVertex2f(330, 135);
+	glVertex2f(385, 185);
+	glVertex2f(355, 240);
+	glEnd();
+
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3f(0.1, 0.1, 0.1);
+	glVertex2f(388, 75);
+	glVertex2f(393, 77);
+	glColor3f(0.2, 0.2, 0.2);
+	glVertex2f(398, 80);
+	glColor3f(0.3, 0.3, 0.3);
+	glVertex2f(385, 185);
+	glVertex2f(330, 135);
+	glEnd();
+
+	//waist right
+	glColor3f(0.96, 0.80, 0.69);
+	glBegin(GL_POLYGON);
+	glVertex2f(355, 240);
+	glVertex2f(385, 185);
+	glVertex2f(390, 240);
+	//glVertex2f(390, 77);
+	glEnd();
+
+	//mid left crease
+	glColor3f(0, 0, 0);
+	glBegin(GL_POLYGON);
+	glVertex2f(280, 215);
+	glVertex2f(288, 200);
+	glVertex2f(320, 155);
+	glVertex2f(360, 135);
+	glVertex2f(330, 170);
+	glEnd();
+
+	//mid right crease
+	glColor3f(0, 0, 0);
+	glBegin(GL_POLYGON);
+	glVertex2f(310, 215);
+	glVertex2f(330, 190);
+	glVertex2f(360, 160);
+	glVertex2f(348, 190);
+	glEnd();
+	int n = 16;
+
+	//hhair
+	glColor3f(0.2, 0.2, 0.2);
+	glBegin(GL_POLYGON);
+
+	for (angle = 1.0; angle < 361.0; angle += 0.2)
+	{
+		x2 = 320 + sin(angle) * 120;
+		y2 = 350 + cos(angle) * 130;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+
+	for (float i = 1; i <= n; i++) {
+		x1 = 320 + 110 * sin((i * 360 / n) * DEG2RAD);
+		y1 = 350 + 120 * cos((i * 360 / n) * DEG2RAD);
+		glBegin(GL_POLYGON);
+		for (angle = 1.0f; angle < 361.0f; angle += 0.2)
+		{
+			x2 = x1 + sin(angle) * 25;
+			y2 = y1 + cos(angle) * 25;
+			glVertex2f(x2, y2);
+		}
+		glEnd();
+	}
+	//hface
+	drawfaceh();
+	//hbeard
+	glColor3f(0, 0, 0);
+	glBegin(GL_POLYGON);
+
+	for (angle = 90; angle < 270; angle += 0.2)
+	{
+		x2 = 320 + sin(angle * DEG2RAD) * 95;
+		y2 = 330 + cos(angle * DEG2RAD) * 100;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+
+	for (float i = 1; i <= n / 2 - 1; i++) {
+		//printf("%f\n",(i * 360 / n));
+		x1 = 320 + 80 * sin((90 + i * 360 / n) * DEG2RAD);
+		y1 = 330 + 95 * cos((90 + i * 360 / n) * DEG2RAD);
+		//glVertex2f(x1, y1);
+		glBegin(GL_POLYGON);
+		for (angle = 1.0f; angle < 361.0f; angle += 0.2)
+		{
+			x2 = x1 + sin(angle) * 35;
+			y2 = y1 + cos(angle) * 35;
+			glVertex2f(x2, y2);
+		}
+		glEnd();
+	}
+	//heyes
+	glColor3f(0, 0, 0);
+	glBegin(GL_POLYGON);
+	for (angle = 1.0; angle < 361.0; angle += 0.2)
+	{
+		x2 = 280 + cos(angle) * 10;
+		y2 = 350 + sin(angle) * 10;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+	glBegin(GL_POLYGON);
+	for (angle = 1.0; angle < 361.0; angle += 0.2)
+	{
+		x2 = 355 + cos(angle) * 10;
+		y2 = 350 + sin(angle) * 10;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+	//hmouth
+	glColor3f(0.96, 0.80, 0.69);
+	glBegin(GL_POLYGON);
+	for (angle = 1.0; angle < 361.0; angle += 0.2)
+	{
+		x2 = 317.5 + cos(angle * DEG2RAD) * 30;
+		y2 = 295 + sin(angle * DEG2RAD) * 20;
+		glVertex2f(x2, y2);
+	}
+
+	glEnd();
+	drawmouth();
+
+	glLoadIdentity();
+	glFlush();
+}
+
 void drawgrass()
 {
 	glColor3f(0, 1, 0);
@@ -574,68 +924,101 @@ void drawgrass()
 	glEnd();
 	glFlush();
 }
+void drawcirc(float x, float y, float z)
+{
+	glTranslatef(x, y, z);
+	float angle, x2, y2;
+	glColor3f(1, 1, 1);
+	glBegin(GL_POLYGON);
+	for (angle = 1.0; angle < 361.0; angle += 0.2)
+	{
+		x2 = 280 + cos(angle) * 5;
+		y2 = 350 + sin(angle) * 5;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+	glLoadIdentity();
+}
 
 void display() {
-	glClearColor(0.567320-c5, 0.806536-c5, 0.906536-c5, 0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	drawgrass();
-	drawzeus();
-	if (f1 == true&& ctr%2==0)
-	{
-		glScalef(1, 0.75, 0.75);
-		glTranslatef(-100, 270, 0);
-		drawlightning();
-		secondln();
-		thirdln();
-		
-		
-		glLoadIdentity();
+	if (pages == 1) {
+		glClearColor(0, 0, 0, 0);
+		glClear(GL_COLOR_BUFFER_BIT);
+		for (int i = 0; i < 30; i++)
+		{
+			float v1, v2;
+			v1 = rand() % 1920;
+			v2 = rand() % 1080;
+			drawcirc(-250 + v1, -300 + v2, 0);
+		}
+		drawzeus();
+		drawhades();
+		glutSwapBuffers();
+		glFlush();
+	}
+	else if (pages == 2) {
+		glClearColor(0.567320 - c5, 0.806536 - c5, 0.906536 - c5, 0);
+		glClear(GL_COLOR_BUFFER_BIT);
+		drawgrass();
+		drawzeus();
+		drawbox1();
+		if (f1 == true && ctr % 2 == 0)
+		{
+			glScalef(1, 0.75, 0.75);
+			glTranslatef(-100, 270, 0);
+			drawlightning();
+			secondln();
+			thirdln();
 
-		if (switchs == 5) {
-			reset_ln();
-		}
-	}
-	if (f1 == true && ctr%3==0)
-	{
-		glScalef(1, 0.75, 0.75);
-		glTranslatef(720, 270, 0);
-		drawlightning();
-		secondln();
-		thirdln();
-		glLoadIdentity();
-		if (switchs == 5) {
-			reset_ln();
-		}
-	}
-	if (f1 == true)
-	{
-		glScalef(1, 0.75, 0.75);
-		glTranslatef(5720, 5270, 0);
-		drawlightning();
-		secondln();
-		thirdln();
-		if (switchs == 4) {
-			alpha -= 0.05;
-			if (alpha < 0.1) {
-				ctr++;
+
+			glLoadIdentity();
+
+			if (switchs == 5) {
 				reset_ln();
-				switchs = 5;
 			}
-		}		
-		glLoadIdentity();
-		if (switchs == 5) {			
-			reset_ln();
 		}
-	}	
-	glColor3f(1 - C4, 1 - C4, 1 - C4);
-	drawclouds(550, 1510 - ypos, 0.80, 0.80);
-	drawclouds(770, 1510 - ypos, 1, 1);
-	drawclouds(1480, 1510 - ypos, 0.80, 0.80);
-	drawclouds(1600, 1510 - ypos, 1, 1);
-	drawclouds(2450, 1510 - ypos, 0.80, 0.80);
-	glutSwapBuffers();
-	glFlush();
-
+		if (f1 == true && ctr % 3 == 0)
+		{
+			glScalef(1, 0.75, 0.75);
+			glTranslatef(720, 270, 0);
+			drawlightning();
+			secondln();
+			thirdln();
+			glLoadIdentity();
+			if (switchs == 5) {
+				reset_ln();
+			}
+		}
+		if (f1 == true)
+		{
+			glScalef(1, 0.75, 0.75);
+			glTranslatef(5720, 5270, 0);
+			drawlightning();
+			secondln();
+			thirdln();
+			if (switchs == 4) {
+				alpha -= 0.05;
+				if (alpha < 0.1) {
+					ctr++;
+					reset_ln();
+					switchs = 5;
+				}
+			}
+			glLoadIdentity();
+			if (switchs == 5) {
+				reset_ln();
+			}
+		}
+		glColor3f(1 - C4, 1 - C4, 1 - C4);
+		drawclouds(550, 1510 - ypos, 0.80, 0.80);
+		drawclouds(770, 1510 - ypos, 1, 1);
+		drawclouds(1480, 1510 - ypos, 0.80, 0.80);
+		drawclouds(1600, 1510 - ypos, 1, 1);
+		drawclouds(2450, 1510 - ypos, 0.80, 0.80);
+		glutSwapBuffers();
+		glFlush();
+	}
+	
 }
 //void idle() {
 //
